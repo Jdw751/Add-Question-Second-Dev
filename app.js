90234f43ab0ecf63322107f8fc3f049bf0e69ba5
Leress <jdw751@msstate.edu>
2017-01-18 10:19:02 -0500

    fixed ng-switch
    ...

---
+++ app.js
@@ -0,0 +1,8 @@
+var app = angular.module("putInQuestion",[]);
+
+app.controller('QuestionCtrl', function($scope, $interval) {
+  $scope.theTime = new Date().toLocaleTimeString();
+  $interval(function () {
+      $scope.theTime = new Date().toLocaleTimeString();
+  }, 1000);
+});
