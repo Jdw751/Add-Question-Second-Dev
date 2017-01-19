5fb84f3aeb4e0945e099026230295772b820f05d
Leress <jdw751@msstate.edu>
2017-01-19 10:48:48 -0500

    added ui-routing
    replaced ng-switch with ui-routing.

--- app.js
+++ app.js
@@ -1,8 +1,46 @@
-var app = angular.module("putInQuestion",[]);
+var app = angular.module("putInQuestion",['ui.router','multipleSelectApp','multipleChoiceApp']);
 
-app.controller('QuestionCtrl', function($scope, $interval) {
-  $scope.theTime = new Date().toLocaleTimeString();
-  $interval(function () {
-      $scope.theTime = new Date().toLocaleTimeString();
-  }, 1000);
+var select = angular.module('multipleSelectApp',[]);
+var choice = angular.module('multipleChoiceApp',[]);
+
+app.controller('QuestionCtrl', function($scope,$location,$state) {
+	$scope.format = '';
+
+	$scope.go = function go(page){
+		
+		$state.go(page);
+
+		console.log($scope.format);
+		console.log(page);
+	};
+
+	//console.log($scope.format);
+  
 });
+
+
+app.config(function($stateProvider,$urlRouterProvider){
+
+	$urlRouterProvider.otherwise('');
+	
+	$stateProvider
+
+		.state('home', {
+			url: '/',
+			templateUrl: 'index.html'
+		})
+
+		.state('multipleChoice', {
+			url: '/multipleChoice',
+			templateUrl: 'MultipleChoiceView.htm'
+
+		})
+		.state('multipleSelect', {
+			url: '/multipleSelect',
+			templateUrl: 'MultipleSelectView.htm'
+
+		});
+
+});
+
+
